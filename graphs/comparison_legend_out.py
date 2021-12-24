14c88f363f9435eb12757b448549469bf443978b
thiagopx <paixao@gmail.com>
2021-12-24 10:21:49 -0300

    update graphs

--- graphs/comparison_legend_out.py
+++ graphs/comparison_legend_out.py
@@ -1,54 +1,54 @@
 import sys
 import json
-from itertools import product
-import matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 
 from matplotlib import rc
-rc('text', usetex=True)
+
+rc("text", usetex=True)
 # rc('font', family='sans-serif')
 
 import seaborn as sns
-sns.set(context='paper', style='darkgrid', font_scale=7)
 
-datasets = ['D1', 'D2', 'cdip']
-methods = ['proposed', 'proposed-nn', 'paixao', 'liang', 'marques']
+sns.set(context="paper", style="darkgrid", font_scale=7)
+
+datasets = ["D1", "D2", "cdip"]
+methods = ["proposed", "proposed-nn", "paixao", "liang", "marques"]
 records = []
 ks = {}
 for method in methods:
     for dataset in datasets:
-        if method == 'liang' and dataset == 'cdip':
+        if method == "liang" and dataset == "cdip":
             continue
-        complement_fname = '_threads=240_use-mask=True' if method == 'liang' else ''
-        base_dir = 'exp1_proposed' if method == 'proposed' else 'exp3_comparison/{}'.format(method)
-        full_fname = '{}/results/{}{}.json'.format(base_dir, dataset, complement_fname)
-        results = json.load(open(full_fname, 'r'))['data']
+        complement_fname = "_threads=240_use-mask=True" if method == "liang" else ""
+        base_dir = "exp1_proposed" if method == "proposed" else "exp3_comparison/{}".format(method)
+        full_fname = "results/{}/{}{}.json".format(base_dir, dataset, complement_fname)
+        results = json.load(open(full_fname, "r"))["data"]
         ks[dataset] = sorted([int(x) for x in results.keys()])
         last = 0
         for k in ks[dataset]:
-            if method == 'liang' and k > 3:
+            if method == "liang" and k > 3:
                 break
             for run in results[str(k)]:
-                accuracy = run['accuracy']
-                document = run['docs'][0]
+                accuracy = run["accuracy"]
+                document = run["docs"][0]
                 records.append([k, document, dataset, method, 100 * accuracy])
             if k == ks[dataset][-1]:
-                last = results[str(k)][0]['accuracy']
+                last = results[str(k)][0]["accuracy"]
 
         # pertubation on the last point (confidence interval plotting)
-        if method != 'liang':
+        if method != "liang":
             records.append([k, None, dataset, method, 100 * last + 0.001])
             records.append([k, None, dataset, method, 100 * last - 0.001])
 
-records.append([1, None, 'D1', 'Liang', 150]) # hack to force put Liang in the legend
-df = pd.DataFrame.from_records(records, columns=('k', 'document', 'dataset', 'method', 'accuracy'))
+records.append([1, None, "D1", "Liang", 150])  # hack to force put Liang in the legend
+df = pd.DataFrame.from_records(records, columns=("k", "document", "dataset", "method", "accuracy"))
 
 # new names for datasets
 # df['dataset'].replace(datasets_map, inplace=True)
 
-path = 'graphs'
+path = "graphs"
 if len(sys.argv) > 1:
     path = sys.argv[1]
 
@@ -56,43 +56,61 @@ if len(sys.argv) > 1:
 # methods = ['\\textbf{Proposed}', 'Proposed-NN', 'Paixão', 'Liang', 'Marques']
 
 fp = sns.FacetGrid(
-    col='dataset', hue='method', hue_order=methods, data=df,
-    height=17, aspect=1.5, sharex=False, legend_out=False,
-    gridspec_kws=dict(width_ratios=[3, 2, 5])
+    col="dataset",
+    hue="method",
+    hue_order=methods,
+    data=df,
+    height=17,
+    aspect=1.5,
+    sharex=False,
+    legend_out=False,
+    gridspec_kws=dict(width_ratios=[3, 2, 5]),
 )
 # font = font_manager.FontProperties(family='sans-serif', size=22)
 # font = font_manager.FontProperties(size=30)
 # font = font_manager.FontProperties(family='serif', size=22)
-fp = fp.map(sns.lineplot, 'k', 'accuracy', marker='s', ci=95, markersize=40)
+fp = fp.map(sns.lineplot, "k", "accuracy", marker="s", ci=95, markersize=40)
 # fp = fp.add_legend(title='method', prop={'size': 100}, labelspacing=0.2, columnspacing=0.5, ncol=3)
 
-datasets_map = {'D1': '\\textsc{S-Marques}', 'D2': '\\textsc{S-Isri-OCR}', 'cdip': '\\textsc{S-cdip}'}
+datasets_map = {"D1": "\\textsc{S-Marques}", "D2": "\\textsc{S-Isri-OCR}", "cdip": "\\textsc{S-cdip}"}
 for ax, max_val in zip(fp.axes[0], [60, 20, 100]):
     values = [1, 2, 3, 4, 5] + list(range(10, max_val + 1, 5))
-    ax.set_xticks(values)#, minor=False)
+    ax.set_xticks(values)  # , minor=False)
     values = [1, None, None, None, 5] + [(value if value % 10 == 0 else None) for value in range(10, max_val + 1, 5)]
-    ax.set_xticklabels(values, fontdict={'fontsize': 110})
+    ax.set_xticklabels(values, fontdict={"fontsize": 110})
     ax.set_yticks([0, 25, 50, 75, 100])
-    ax.set_yticklabels([0, 25, 50, 75, 100], fontdict={'fontsize': 110})
-    dataset = ax.get_title().replace('dataset = ', '')
-    ax.set_title(datasets_map[dataset], fontdict={'fontsize': 110})
-    ax.set_xlabel('$k$', fontsize=110)
+    ax.set_yticklabels([0, 25, 50, 75, 100], fontdict={"fontsize": 110})
+    dataset = ax.get_title().replace("dataset = ", "")
+    ax.set_title(datasets_map[dataset], fontdict={"fontsize": 110})
+    ax.set_xlabel("$k$", fontsize=110)
 
-fp.axes.flat[0].set_ylabel('accuracy (\%)', fontsize=110)
+fp.axes.flat[0].set_ylabel("accuracy (\%)", fontsize=110)
 fp.set(ylim=(-5, 101))
 
 # change legend labels
-methods_map = {'proposed': '\\textbf{Proposed}', 'proposed-nn': 'Proposed-NN', 'paixao': 'Paixão', 'marques': 'Marques', 'liang': 'Liang'}
+methods_map = {
+    "proposed": "\\textbf{Deeprec-CL}",
+    "proposed-nn": "Deeprec-CL-NN",
+    "paixao": "Paixão",
+    "marques": "Marques",
+    "liang": "Liang",
+}
 # leg = fp.axes.flat[0].get_legend()
 
 first_ax = fp.axes.flat[0]
 leg = first_ax.legend(
-    title='method', loc='center left', bbox_to_anchor=(1.0, -0.5), fontsize=100,
-    title_fontsize=110, labelspacing=0.2, columnspacing=0.5, ncol=3
+    title="method",
+    loc="center left",
+    bbox_to_anchor=(1.0, -0.5),
+    fontsize=100,
+    title_fontsize=110,
+    labelspacing=0.2,
+    columnspacing=0.5,
+    ncol=3,
 )
 for text in leg.get_texts():
     text.set_text(methods_map[text.get_text()])
 # plt.setp(leg.get_title(), fontsize=110)  # legend title size
 
-plt.savefig('{}/comparison_legend_out.pdf'.format(path), bbox_inches='tight')
+plt.savefig("{}/comparison_legend_out.pdf".format(path), bbox_inches="tight")
 # plt.show()
--- graphs/time.py
+++ graphs/time.py
@@ -7,14 +7,16 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as font_manager
 
 from matplotlib import rc
-rc('text', usetex=True)
+
+rc("text", usetex=True)
 # rc('font', family='sans-serif')
 
 import seaborn as sns
-sns.set(context='paper', style='darkgrid', font_scale=7)
+
+sns.set(context="paper", style="darkgrid", font_scale=7)
 # sns.set_style({'font.family': ['sans-serif'], 'sans-serif': ['Arial']})
 
-datasets = ['D1', 'D2']#, cdip']
+datasets = ["D1", "D2"]  # , cdip']
 # datasets_map = {'D1': 'S-Marques', 'D2': 'S-Isri-OCR', 'cdip': 'S-cdip'}
 
 
@@ -26,82 +28,80 @@ datasets = ['D1', 'D2']#, cdip']
 
 records = []
 for dataset in datasets:
-    fname_mat = 'exp1_proposed/results/{}_matrix.json'.format(dataset)
-    fname_res = 'exp1_proposed/results/{}.json'.format(dataset)
-    matrix_data = json.load(open(fname_mat, 'r'))
-    N = sum(matrix_data['sizes'])
+    fname_mat = "results/exp1_proposed/{}_matrix.json".format(dataset)
+    fname_res = "results/exp1_proposed/{}.json".format(dataset)
+    matrix_data = json.load(open(fname_mat, "r"))
+    N = sum(matrix_data["sizes"])
     total_inferences = N * (N - 1)
-    mean_inference_time = matrix_data['comp_time'] / total_inferences # pair evaluation mean time
-    results = json.load(open(fname_res, 'r'))['data']
+    mean_inference_time = matrix_data["comp_time"] / total_inferences  # pair evaluation mean time
+    results = json.load(open(fname_res, "r"))["data"]
     for k in range(1, 6):
         for run in results[str(k)]:
-            N = sum(run['sizes'])
+            N = sum(run["sizes"])
             total_inferences = N * (N - 1)
             comp_time = total_inferences * mean_inference_time
-            total_time = comp_time + run['opt_time']
-            records.append([k, 'proposed', total_time])
+            total_time = comp_time + run["opt_time"]
+            records.append([k, "proposed", total_time])
 
 # marques
 for dataset in datasets:
-    fname_mat = 'exp3_comparison/marques/results/{}_matrix.json'.format(dataset)
-    fname_res = 'exp3_comparison/marques/results/{}.json'.format(dataset)
-    matrix_data = json.load(open(fname_mat, 'r'))
-    N = sum(matrix_data['sizes'])
+    fname_mat = "results/exp3_comparison/marques/{}_matrix.json".format(dataset)
+    fname_res = "results/exp3_comparison/marques/{}.json".format(dataset)
+    matrix_data = json.load(open(fname_mat, "r"))
+    N = sum(matrix_data["sizes"])
     total_inferences = N * (N - 1)
-    mean_inference_time = matrix_data['comp_time'] / total_inferences # pair evaluation mean time
-    results = json.load(open(fname_res, 'r'))['data']
+    mean_inference_time = matrix_data["comp_time"] / total_inferences  # pair evaluation mean time
+    results = json.load(open(fname_res, "r"))["data"]
     for k in range(1, 6):
         for run in results[str(k)]:
-            N = sum(run['sizes'])
+            N = sum(run["sizes"])
             total_inferences = N * (N - 1)
             comp_time = total_inferences * mean_inference_time
-            total_time = comp_time + run['opt_time']
-            records.append([k, 'marques', total_time])
+            total_time = comp_time + run["opt_time"]
+            records.append([k, "marques", total_time])
 
 # paixao
 for dataset in datasets:
-    fname = 'exp3_comparison/paixao/results/{}.json'.format(dataset)
-    results = json.load(open(fname, 'r'))['data']
+    fname = "results/exp3_comparison/paixao/{}.json".format(dataset)
+    results = json.load(open(fname, "r"))["data"]
     for k in range(1, 6):
         for run in results[str(k)]:
-            total_time = run['comp_time'] + run['opt_time']
-            records.append([k, 'paixao', total_time])
+            total_time = run["comp_time"] + run["opt_time"]
+            records.append([k, "paixao", total_time])
 
 # liang
 for dataset in datasets:
-    fname = 'exp3_comparison/liang/results/{}_threads=240_use-mask=True.json'.format(dataset)
-    results = json.load(open(fname, 'r'))['data']
+    fname = "results/exp3_comparison/liang/{}_threads=240_use-mask=True.json".format(dataset)
+    results = json.load(open(fname, "r"))["data"]
     # k_max = 5 if dataset == 'D1' else 3
     k_max = 3
     for k in range(1, k_max + 1):
         for run in results[str(k)]:
-            if run['solution'] != None:
-                total_time = run['opt_time']
-                records.append([k, 'liang', total_time])
+            if run["solution"] != None:
+                total_time = run["opt_time"]
+                records.append([k, "liang", total_time])
 
-df = pd.DataFrame.from_records(records, columns=['k', 'method', 'time'])
+df = pd.DataFrame.from_records(records, columns=["k", "method", "time"])
 
-methods = ['proposed', 'paixao', 'marques', 'liang']
-fp = sns.FacetGrid(
-    hue='method', hue_order=methods, data=df,
-    height=17, aspect=2, legend_out=False, sharey=False
-)
-fp = fp.map(sns.lineplot, 'k', 'time', marker='s', ci=None, markersize=40)
-fp.add_legend(title='method', fontsize=100, labelspacing=0.2) # bug in title_fontsize
+methods = ["proposed", "paixao", "marques", "liang"]
+fp = sns.FacetGrid(hue="method", hue_order=methods, data=df, height=17, aspect=2, legend_out=False, sharey=False)
+fp = fp.map(sns.lineplot, "k", "time", marker="s", ci=None, markersize=40)
+fp.add_legend(title="method", fontsize=100, labelspacing=0.1, columnspacing=0.1, ncol=2)  # bug in title_fontsize
 fp.set(yticks=(0, 2000, 4000, 6000), xticks=list(range(1, 6)), ylim=(-200, 6200))
-fp.ax.set_xlabel('$k$', fontdict={'fontsize': 110})
-fp.ax.set_ylabel('time (s)', fontdict={'fontsize': 110})
-fp.set_yticklabels((0, 2000, 4000, 6000), fontdict={'fontsize': 110})
-fp.set_xticklabels(list(range(1, 6)), fontdict={'fontsize': 110})
+fp.ax.set_xlabel("$k$", fontdict={"fontsize": 110})
+fp.ax.set_ylabel("time (s)", fontdict={"fontsize": 110})
+fp.set_yticklabels((0, 2000, 4000, 6000), fontdict={"fontsize": 110})
+fp.set_xticklabels(list(range(1, 6)), fontdict={"fontsize": 110})
 
 # figure 1 - all methods
 # legend adjustment
-methods_map = {'proposed': '\\textbf{Proposed}', 'paixao': 'Paixão', 'marques': 'Marques', 'liang': 'Liang'}
+methods_map = {"proposed": "\\textbf{Deeprec-CL}", "paixao": "Paixão", "marques": "Marques", "liang": "Liang"}
 leg = fp.ax.get_legend()
-
+print(leg.__dict__)
 for text in leg.get_texts():
     text.set_text(methods_map[text.get_text()])
-plt.setp(leg.get_title(), fontsize=110)  # legend title size
+    text.set_size(90)
+plt.setp(leg.get_title(), fontsize=100)  # legend title size
 
 bb = leg.get_bbox_to_anchor().inverse_transformed(fp.ax.transAxes)
 bb.y0 += 0.035
@@ -110,21 +110,18 @@ bb.x0 -= 0.015
 bb.x1 -= 0.015
 leg.set_bbox_to_anchor(bb, transform=fp.ax.transAxes)
 
-path = 'graphs'
+path = "graphs"
 if len(sys.argv) > 1:
     path = sys.argv[1]
-plt.savefig('{}/time1.pdf'.format(path), bbox_inches='tight')
+plt.savefig("{}/time1.pdf".format(path), bbox_inches="tight")
 
-# figure 2 - only proposed and marques
-df = df[df['method'].isin(['marques', 'proposed'])]
-fp = sns.FacetGrid(
-    hue='method', hue_order=methods, data=df,
-    height=17, aspect=1, legend_out=False, sharey=False
-)
-fp = fp.map(sns.lineplot, 'k', 'time', marker='s', ci=None, markersize=40)
-fp.set(yticks=(0, 100, 200, 300), xticks=list(range(1, 6)), ylim=(-10, 360))
-fp.ax.set_xlabel('$k$', fontdict={'fontsize': 110})
-fp.ax.set_ylabel('time (s)', fontdict={'fontsize': 110})
-fp.set_yticklabels((0, 100, 200, 300), fontdict={'fontsize': 110})
-fp.set_xticklabels(list(range(1, 6)), fontdict={'fontsize': 110})
-plt.savefig('{}/time2.pdf'.format(path), bbox_inches='tight')
+# # figure 2 - only proposed and marques
+# df = df[df["method"].isin(["marques", "proposed"])]
+# fp = sns.FacetGrid(hue="method", hue_order=methods, data=df, height=17, aspect=1, legend_out=False, sharey=False)
+# fp = fp.map(sns.lineplot, "k", "time", marker="s", ci=None, markersize=40)
+# fp.set(yticks=(0, 100, 200, 300), xticks=list(range(1, 6)), ylim=(-10, 360))
+# fp.ax.set_xlabel("$k$", fontdict={"fontsize": 110})
+# fp.ax.set_ylabel("time (s)", fontdict={"fontsize": 110})
+# fp.set_yticklabels((0, 100, 200, 300), fontdict={"fontsize": 110})
+# fp.set_xticklabels(list(range(1, 6)), fontdict={"fontsize": 110})
+# plt.savefig("{}/time2.pdf".format(path), bbox_inches="tight")
